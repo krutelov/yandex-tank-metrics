8529be446453150fc349691b83ac65a06f47133d
krutelov <krutelevcode@gmail.com>
2017-10-11 17:23:52 +0300

    Added chart. Added libraries.

--- Metrics/static/js/script.js
+++ Metrics/static/js/script.js
@@ -69,6 +69,40 @@ var test_data;
 
         return chart;
     });
+    nv.addGraph(function() {
+        var chart;
+        chart = nv.models.lineChart()
+            .options({
+                duration: 300,
+                useInteractiveGuideline: true
+            })
+        ;
+
+        chart.xAxis.tickFormat(function(d) {
+            return d3.time.format('%H:%M:%S')(new Date(d))
+        }).showMaxMin(false);
+
+        chart.padData(true)
+
+        chart.yAxis
+            .axisLabel('ms')
+            .tickFormat(function(d) {
+                if (d == null) {
+                    return 'N/A';
+                }
+                return d3.format(',.2f')(d);
+            })
+        ;
+
+        netCodeData = netCode();
+
+        d3.select('#chart3').append('svg')
+            .datum(netCodeData)
+            .call(chart);
+        nv.utils.windowResize(chart.update);
+
+        return chart;
+    });
 
     function Quantiles() {
         var curve98 = [],
@@ -85,11 +119,10 @@ var test_data;
             for (var i = 0; i < test_data.length; i++) {
                 if(0 < test_data[i].data.overall.interval_real.hist.bins.length){
                     for(var j = 0; j < test_data[i].data.overall.interval_real.hist.bins.length; j++){
-                        Distribution.push({test_data: test_data[i].data.overall.interval_real.hist.data[j], bins: test_data[i].data.overall.interval_real.hist.bins[j]});
+                        Distribution.push({data: test_data[i].data.overall.interval_real.hist.data[j], bins: test_data[i].data.overall.interval_real.hist.bins[j]});
                     }
                 }
             }
-            console.log(Distribution[0].bins);
             function compareAge(personA, personB) {
               return personA.bins - personB.bins;
             }
@@ -100,30 +133,28 @@ var test_data;
                 if (Distribution[i].bins == Distribution[i-1].bins) {
                     dataDistribution += Distribution[i-1].data;
                 }else if(Distribution[i].bins != Distribution[i-1].bins || Distribution[i].bins != Distribution[i+1].bins){
-                    Distribution2.push({test_data: Distribution[i].data , bins: Distribution[i].bins });
+                    Distribution2.push({data: Distribution[i].data , bins: Distribution[i].bins });
                     $(".distributio").append('<tr><td>' + Distribution[i].data + '</td><td>' + Distribution[i].bins + '</td></tr>');
-                    console.log(dataDistribution + " --- " + Distribution[i].bins + "  -----------  " + i)
                     i++;
                     dataDistribution = 0;
                 }else{
                     dataDistribution += Distribution[i].data;
-                    Distribution2.push({test_data: dataDistribution , bins: Distribution[i-1].bins });
+                    Distribution2.push({data: dataDistribution , bins: Distribution[i-1].bins });
                     $(".distributio").append('<tr><td>' + dataDistribution + '</td><td>' + Distribution[i-1].bins + '</td></tr>');
                     dataDistribution = 0;
                 }
             }
-            Distribution2.push({test_data: dataDistribution , bins: Distribution[Distribution.length-1].bins });
+            Distribution2.push({data: dataDistribution , bins: Distribution[Distribution.length-1].bins });
             $(".distributio").append('<tr><td>' + dataDistribution + '</td><td>' + Distribution[Distribution.length-1].bins + '</td></tr>');
         }
         Distribution()
 
-        console.log(test_data[0].data.overall.interval_real.q.value);
         for (var i = 0; i < test_data.length; i++) {
             Quantiles.push({ts: test_data[i].stats.ts, value : test_data[i].data.overall.interval_real.q.value});
         }
 
         for (var i = 0; i < Quantiles.length; i++) {
-            
+
             curve50.push({x: Quantiles[i].ts*1000, y: Quantiles[i].value[0]});
             curve75.push({x: Quantiles[i].ts*1000, y: Quantiles[i].value[1]});
             curve80.push({x: Quantiles[i].ts*1000, y: Quantiles[i].value[2]});
@@ -132,7 +163,6 @@ var test_data;
             curve95.push({x: Quantiles[i].ts*1000, y: Quantiles[i].value[5]});
             curve98.push({x: Quantiles[i].ts*1000, y: Quantiles[i].value[6]});
         }
-        //console.log(sin2);
 
         return [
             {
@@ -210,141 +240,117 @@ var test_data;
         ];
     }
     function httpCode() {
-        var cod200 = [],
-            cod500 = [],
-            cod404 = [],
-            cod0 = []
-            ;
         var httpCode = [];
-        
-        
+        var httpCodeGraf = [];
+        var obj = {};
+        function get_random_color() {
+        var randomcolor;
+            randomcolor="#"+((1<<24)*Math.random()|0).toString(16);
+            return randomcolor;
+        }
         for (var i = 0; i < test_data.length; i++) {
-
-            function prov(p){
-                if(test_data[i].data.overall.proto_code.count[p] != undefined) return test_data[i].data.overall.proto_code.count[p];
-                else return 0;
+            if (Object.keys(test_data[i].data.overall.proto_code["count"]).length == 1) {
+                var str = Object.keys(test_data[i].data.overall.proto_code["count"]);
+                obj[str] = "true333";
+            }else{
+                for(var j = 0; j < Object.keys(test_data[i].data.overall.proto_code["count"]).length; j++){
+                    var str = Object.keys(test_data[i].data.overall.proto_code["count"])[j];
+                    obj[str] = true;
+                }
             }
-            httpCode.push({ts: test_data[i].stats.ts, value :[prov(200), prov(404), prov(500), prov(0)]});
-
         }
-        console.log(httpCode);
-        for (var i = 0; i < httpCode.length; i++) {
-            cod200.push({x: httpCode[i].ts*1000, y: httpCode[i].value[0]});
-            cod500.push({x: httpCode[i].ts*1000, y: httpCode[i].value[1]});
-            cod404.push({x: httpCode[i].ts*1000, y: httpCode[i].value[2]});
-            cod0.push({x: httpCode[i].ts*1000, y: httpCode[i].value[3]});
-        }
-        console.log(cod500);
 
-        return [
-            {
-                area: true,
-                values: cod200,
-                key: "cod200",
-                color: "#00ff39",
-                fillOpacity: .1
-            },
-            {
-                area: true,
-                values: cod500,
-                key: "cod500",
-                color: "#7ffc8a",
-                fillOpacity: .1
-            },
-            {
-                area: true,
-                values: cod404,
-                key: "cod404",
-                color: "#fffa36",
-                fillOpacity: .1
-            },
-            {
-                area: true,
-                values: cod0,
-                key: "cod0",
-                color: "#fffa36",
-                fillOpacity: .1
+        var arr = Object.keys(obj);
+        var bar = String(arr).split(',');
+        function httpCodePush1(j){
+            var ob = [];
+            for (var i = 0; i < Object.keys(obj).length; i++) {
+                if (Object.keys(test_data[j].data.overall.proto_code["count"]).length == 1) {
+                    if (bar[i] == Object.keys(test_data[j].data.overall.proto_code["count"])) {
+                        ob.push(test_data[j].data.overall.proto_code["count"][bar[i]]);
+                    }else{
+                        ob.push(0);
+                    }
+                }else{
+                    for(var p = 0; p < Object.keys(test_data[j].data.overall.proto_code["count"]).length; p++){
+                        //console.log(Object.keys(test_data[j].data.overall.net_code["count"]));
+                    }
+                }
+
             }
-            /*{
-                values: cos,
-                key: "Cosine Wave",
-                color: "#2ca02c"
-            },
-            {
-                values: rand,
-                key: "Random Points",
-                color: "#2222ff"
-            },
-            {
-                values: rand2,
-                key: "Random Cosine",
-                color: "#667711",
-                strokeWidth: 3.5
-            },
-            {
-                area: true,
-                values: sin2,
-                key: "Fill opacity",
-                color: "#EF9CFB",
-                fillOpacity: .1
-            }*/
-        ];
+            return ob;
+        }
+        for (var i = 0; i < test_data.length; i++) {
+            httpCode.push({ts: test_data[i].stats.ts, value : httpCodePush1(i)});
+        }
+        function linehttpCode (bar, o){
+            lineObj = [];
+            for (var i = 0; i < httpCode.length; i++) {
+                lineObj.push({x: httpCode[i].ts*1000, y: httpCode[i].value[o]});
+            }
+            return {area: true, values: lineObj, key: "cod_" + bar, color: get_random_color(), fillOpacity: .1};
+        }
+        for (var i = 0; i < bar.length; i++) {
+            httpCodeGraf.push(linehttpCode(bar[i], i));
+        }
+
+        return httpCodeGraf;
     }
     function netCode() {
-        var cod200 = [],
-            cod500 = [],
-            cod404 = [],
-            cod0 = []
-            ;
-        var httpCode = [];
-        
-        
+        var netCode = [];
+        var netCodeGraf = [];
+        var obj = {};
+        function get_random_color() {
+        var randomcolor;
+            randomcolor="#"+((1<<24)*Math.random()|0).toString(16);
+            return randomcolor;
+        }
         for (var i = 0; i < test_data.length; i++) {
-
-            function prov(p){
-                if(test_data[i].data.overall.proto_code.count[p] != undefined) return test_data[i].data.overall.proto_code.count[p];
-                else return 0;
+            if (Object.keys(test_data[i].data.overall.net_code["count"]).length == 1) {
+                var str = Object.keys(test_data[i].data.overall.net_code["count"]);
+                obj[str] = "true333";
+            }else{
+                for(var j = 0; j < Object.keys(test_data[i].data.overall.net_code["count"]).length; j++){
+                    var str = Object.keys(test_data[i].data.overall.net_code["count"])[j];
+                    obj[str] = true;
+                }
             }
-            httpCode.push({ts: test_data[i].stats.ts, value :[prov(200), prov(404), prov(500), prov(0)]});
-
         }
-        console.log(httpCode);
-        for (var i = 0; i < httpCode.length; i++) {
-            cod200.push({x: httpCode[i].ts*1000, y: httpCode[i].value[0]});
-            cod500.push({x: httpCode[i].ts*1000, y: httpCode[i].value[1]});
-            cod404.push({x: httpCode[i].ts*1000, y: httpCode[i].value[2]});
-            cod0.push({x: httpCode[i].ts*1000, y: httpCode[i].value[3]});
-        }
-        console.log(cod500);
 
-        return [
-            {
-                area: true,
-                values: cod200,
-                key: "cod200",
-                color: "#00ff39",
-                fillOpacity: .1
-            },
-            {
-                area: true,
-                values: cod500,
-                key: "cod500",
-                color: "#7ffc8a",
-                fillOpacity: .1
-            },
-            {
-                area: true,
-                values: cod404,
-                key: "cod404",
-                color: "#fffa36",
-                fillOpacity: .1
-            },
-            {
-                area: true,
-                values: cod0,
-                key: "cod0",
-                color: "#fffa36",
-                fillOpacity: .1
+        var arr = Object.keys(obj);
+        var bar = String(arr).split(',');
+        function netCodPush1(j){
+            var ob = [];
+            for (var i = 0; i < Object.keys(obj).length; i++) {
+                if (Object.keys(test_data[j].data.overall.net_code["count"]).length == 1) {
+                    if (bar[i] == Object.keys(test_data[j].data.overall.net_code["count"])) {
+                        ob.push(test_data[j].data.overall.net_code["count"][bar[i]]);
+                    }else{
+                        ob.push(0);
+                    }
+                }else{
+                    for(var p = 0; p < Object.keys(test_data[j].data.overall.net_code["count"]).length; p++){
+                        //console.log(Object.keys(test_data[j].data.overall.net_code["count"]));
+                    }
+                }
+
             }
-        ];
+            return ob;
+        }
+        for (var i = 0; i < test_data.length; i++) {
+            netCode.push({ts: test_data[i].stats.ts, value : netCodPush1(i)});
+        }
+        function lineCod (bar, o){
+            lineObj = [];
+            for (var i = 0; i < netCode.length; i++) {
+                lineObj.push({x: netCode[i].ts*1000, y: netCode[i].value[o]});
+            }
+            return {area: true, values: lineObj, key: "net_" + bar, color: get_random_color(), fillOpacity: .1};
+        }
+        for (var i = 0; i < bar.length; i++) {
+            netCodeGraf.push(lineCod(bar[i], i));
+        }
+
+        return netCodeGraf;
     }
+
